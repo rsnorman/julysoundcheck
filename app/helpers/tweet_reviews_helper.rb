20a18a91af4e72a4275c988960979ef4c79a716c
Ryan Norman <rsnorman15@gmail.com>
2016-07-09 09:15:03 -0400

    Allow clearing out of rating

--- app/helpers/tweet_reviews_helper.rb
+++ app/helpers/tweet_reviews_helper.rb
@@ -1,6 +1,6 @@
 module TweetReviewsHelper
   def rating_options
-    Rating::RATINGS.to_a.inject({}) do |ratings, rating|
+    Rating::RATINGS.to_a.inject({'-- Choose Rating --' => nil}) do |ratings, rating|
       ratings.merge("#{Rating::DESCRIPTIONS[rating.last]} (#{rating.first})" => rating.last)
     end
   end
