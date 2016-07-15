c261ccdd772864cfdcebdfec812432b956697430
Ryan Norman <rsnorman15@gmail.com>
2016-07-15 01:54:03 -0400

    Add all current tweets to old repo

--- db/migrate/20160714020404_tweets.rb
+++ db/migrate/20160714020404_tweets.rb
@@ -4,7 +4,7 @@ class Tweets < ActiveRecord::Migration[5.0]
       t.string  :tweet_id,                null: false
       t.string   :screen_name,            null: false
       t.string   :name,                   null: false
-      t.string   :in_reply_to_status_id,  null: false
+      t.string   :in_reply_to_status_id
       t.string   :profile_image_uri,      null: false
       t.text     :text,                   null: false
       t.boolean  :is_review,              null: false, default: false
