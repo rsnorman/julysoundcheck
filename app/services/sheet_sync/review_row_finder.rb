dae4d2adb20f99487d546eaca1ac9ee6dd4cf4bc
Ryan Norman <rsnorman15@gmail.com>
2016-07-16 16:46:28 -0400

    All upload sync to spreadsheet with new database tweet models

--- app/services/sheet_sync/review_row_finder.rb
+++ app/services/sheet_sync/review_row_finder.rb
@@ -8,9 +8,22 @@ module SheetSync
 
     def find(tweet_review)
       worksheet.each_row do |row|
-        return row if row.tweet(with_formula: true).include?("/status/#{tweet_review.tweet_id}")
+        return row if includes_tweet_review?(tweet_review, row)
       end
       nil
+    end
+
+    private
+
+    def includes_tweet_review?(tweet_review, row)
+      tweet_cell = row.tweet(with_formula: true)
+      tweet_link?(tweet_review.tweet, tweet_cell) ||
+        tweet_link?(tweet_review.tweet.reply, tweet_cell)
+    end
+
+    def tweet_link?(tweet, cell_content)
+      return false unless tweet
+      cell_content.include?("/status/#{tweet.tweet_id}")
     end
   end
 end
