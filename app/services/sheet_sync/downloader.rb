b95093771f5f3741d89cbaee4888c9d72fc03c71
Ryan Norman <rsnorman15@gmail.com>
2016-07-23 09:14:04 -0400

    Don't assume tweet is present when syncing review

--- app/services/sheet_sync/downloader.rb
+++ app/services/sheet_sync/downloader.rb
@@ -54,7 +54,7 @@ module SheetSync
         listen_url: parse_link(row.source(with_formula: true)),
         twitter_status_id: tweet_id,
         rating: Rating.from_score(row.rating).value,
-        tweet: tweet.in_reply_to_tweet || tweet
+        tweet: tweet.try(:in_reply_to_tweet) || tweet
       }.keep_if { |_attr_name, attr_value| !attr_value.blank? }
     end
 
