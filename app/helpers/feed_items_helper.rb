91232755e67e1296089e68050c758291871e123b
Ryan Norman <rsnorman15@gmail.com>
2016-07-24 23:19:28 -0400

    Don't use slow constantize for presenter

--- app/helpers/feed_items_helper.rb
+++ app/helpers/feed_items_helper.rb
@@ -1,6 +1,16 @@
 module FeedItemsHelper
   def feed_item_locals(feed_item)
-    presenter = "JulySoundcheck#{feed_item.feedable_type}".constantize
+    presenter = july_soundcheck_presenter_for(feed_item.feedable_type)
     {feed_item.feedable_type.underscore.to_sym => presenter.new(feed_item.feedable, feed_item)}
+  end
+
+  private
+
+  def july_soundcheck_presenter_for(feedable_type)
+    if feedable_type == 'Tweet'
+      JulySoundcheckTweet
+    else
+      JulySoundcheckTweetReview
+    end
   end
 end
