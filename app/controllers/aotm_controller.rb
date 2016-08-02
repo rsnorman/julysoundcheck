dae94b5443f6f5d0a7c946700abfa89c082f5569
rsnorman <rsnorman15@gmail.com>
2016-08-01 22:27:55 -0400

    Adds filter for AOTM (#55)

---
+++ app/controllers/aotm_controller.rb
@@ -0,0 +1,17 @@
+class AotmController < ApplicationController
+  before_action :set_reviews, only: :index
+  before_action :set_review_feed_items, only: :index
+
+  private
+
+  def set_reviews
+    @reviews = TweetReview
+      .where(album_of_the_month: true)
+      .page(params[:page])
+      .order('tweet_reviews.rating DESC, tweet_reviews.created_at DESC')
+  end
+
+  def set_review_feed_items
+    @feed_items = @reviews.map(&:feed_item).compact
+  end
+end
