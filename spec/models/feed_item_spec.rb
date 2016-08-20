8999836e95f5c126b254d68fbb3524147623c552
Ryan Norman <rsnorman15@gmail.com>
2016-08-20 10:34:38 -0400

    Adds model specs for feed item and tweet

---
+++ spec/models/feed_item_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe FeedItem do
+  it { should belong_to(:feedable) }
+  it { should belong_to(:user) }
+
+
+  #it { should validate_uniqueness_of(:feedable_id).scoped_to(:feedable_type) }
+end
+
