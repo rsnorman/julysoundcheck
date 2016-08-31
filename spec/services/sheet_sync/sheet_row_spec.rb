5ceb2c99bebdebd4c4c4feafbcc03d905f9738cc
Ryan Norman <rsnorman15@gmail.com>
2016-08-30 22:54:07 -0400

    Adds sheet row spec

---
+++ spec/services/sheet_sync/sheet_row_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+require './app/services/sheet_sync/sheet_row'
+
+RSpec.describe SheetSync::SheetRow do
+  let(:worksheet) { double('Worksheet') }
+  let(:row_index) { 2 }
+
+  subject { described_class.new(worksheet, row_index) }
+
+  described_class::ATTRIBUTE_COLUMNS.each_pair do |name, col_index|
+
+    describe '#artist' do
+      it 'retrieves value from sheet' do
+        expect(worksheet).to receive(:value).with(row_index, col_index)
+        subject.public_send(name)
+      end
+
+      context 'retrieving formula value' do
+        it 'retrieves value from sheet' do
+          expect(worksheet)
+            .to receive(:formula_value)
+            .with(row_index, col_index)
+          subject.public_send(name, with_formula: true)
+        end
+      end
+    end
+
+    describe "##{name}=" do
+      it 'sets the value' do
+        expect(worksheet)
+          .to receive(:set_value)
+          .with(row_index, col_index, 'Husker Du')
+        subject.public_send("#{name}=", 'Husker Du')
+      end
+    end
+  end
+
+  describe '#tweet' do
+    it 'returns review value' do
+      expect(worksheet)
+        .to receive(:value)
+        .with(row_index, described_class::ATTRIBUTE_COLUMNS[:review])
+      subject.tweet
+    end
+  end
+end
