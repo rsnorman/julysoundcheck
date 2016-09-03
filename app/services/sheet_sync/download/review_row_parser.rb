ab5f5f89b504bb4d19076ffec64ec300f01fc333
Ryan Norman <rsnorman15@gmail.com>
2016-09-02 23:18:26 -0400

    Adds specs for review row parser"

--- app/services/sheet_sync/download/review_row_parser.rb
+++ app/services/sheet_sync/download/review_row_parser.rb
@@ -1,30 +1,34 @@
 module SheetSync
   module Download
     class ReviewRowParser
-      def initialize(row)
+      def initialize(row, rating_score_convertor: Rating)
         @row = row
+        @rating_score_convertor = rating_score_convertor
       end
 
       def parse
-        reviewed_at = Date.strptime(row.date_reviewed, '%m/%d/%Y')
-        reviewed_at = Time.current if Date.today == reviewed_at
-
         {
           artist: row.artist,
           album: row.album,
           genre: row.genre,
           listen_url: parse_link(row.source(with_formula: true)),
-          rating: Rating.from_score(row.rating).value,
+          rating: @rating_score_convertor.from_score(row.rating).value,
           text: row.review,
           reviewed_at: reviewed_at,
           album_of_the_month: !row.aotm.blank?,
           reviewer: row.reviewer
-        }.keep_if { |_attr_name, attr_value| !attr_value.blank? }
+        }.keep_if { |_attr_name, attr_value| !attr_value.nil? && attr_value != '' }
       end
 
       private
 
       attr_reader :row
+
+      def reviewed_at
+        @reviewed_at = Date.strptime(row.date_reviewed, '%m/%d/%Y')
+        @reviewed_at = Time.current if Date.today == @reviewed_at
+        @reviewed_at
+      end
 
       def parse_link(cell_formula)
         cell_formula.split('"').second
---
+++ spec/services/sheet_sync/download/review_row_parser_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe SheetSync::Download::ReviewRowParser do
+  describe '#parse' do
+    let(:row) do
+      double('SheetRow', artist: 'The Babies',
+                         album: 'Our House On The Hill',
+                         genre: 'Indie Rock',
+                         rating: '3-',
+                         source: '=HYPERLINK("http://spotify.com/12345","Spotify")',
+                         review: review,
+                         aotm: aotm,
+                         reviewer: 'Kevin Morby',
+                         date_reviewed: date_reviewed).as_null_object
+    end
+    let(:review) { 'Punchy indie rock!' }
+    let(:date_reviewed) { '6/1/2016' }
+    let(:aotm) { '' }
+    let(:rating) { double('Rating', value: 9) }
+    let(:rating_score_convertor) do
+      double('RatingScoreConvertor').tap do |rsc|
+        allow(rsc).to receive(:from_score).with('3-').and_return rating
+      end
+    end
+
+    subject do
+      described_class.new(row, rating_score_convertor: rating_score_convertor)
+    end
+
+    it 'returns artist attribute' do
+      expect(subject.parse).to include(artist: 'The Babies')
+    end
+
+    it 'returns album attribute' do
+      expect(subject.parse).to include(album: 'Our House On The Hill')
+    end
+
+    it 'returns genre attribute' do
+      expect(subject.parse).to include(genre: 'Indie Rock')
+    end
+
+    it 'returns listen_url attribute' do
+      expect(subject.parse).to include(listen_url: 'http://spotify.com/12345')
+    end
+
+    it 'returns rating attribute' do
+      expect(subject.parse).to include(rating: 9)
+    end
+
+    it 'returns text attribute' do
+      expect(subject.parse).to include(text: 'Punchy indie rock!')
+    end
+
+    it 'returns reviewed_at attribute' do
+      expect(subject.parse).to include(reviewed_at: Date.new(2016, 6, 1))
+    end
+
+    it 'returns album_of_the_month attribute' do
+      expect(subject.parse).to include(album_of_the_month: false)
+    end
+
+    it 'returns reviewer attribute' do
+      expect(subject.parse).to include(reviewer: 'Kevin Morby')
+    end
+
+    context 'with album of the month cell containing text' do
+      let(:aotm) { 'Whoop, whoop!' }
+
+      it 'returns album_of_the_month attribute as true' do
+        expect(subject.parse).to include(album_of_the_month: true)
+      end
+    end
+
+    context 'with reviewed_at equal to current day' do
+      let(:date_reviewed) { Date.current.strftime('%m/%d/%Y') }
+
+      it 'returns reviewed_at attribute set to current time' do
+        expect(subject.parse[:reviewed_at])
+          .to be_within(1.second).of(Time.current)
+      end
+    end
+
+    context 'with blank values in row cell' do
+      let(:review) { '' }
+
+      it 'doesn\'t include empty attributes' do
+        expect(subject.parse).not_to have_key(:text)
+      end
+    end
+
+    context 'with nil values in row cell' do
+      let(:review) { nil }
+
+      it 'doesn\'t include empty attributes' do
+        expect(subject.parse).not_to have_key(:text)
+      end
+    end
+  end
+end
