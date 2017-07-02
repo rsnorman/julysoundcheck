bfe7bcb0bf0f272e123a0d0134810b498e199cc2
Ryan Norman <rsnorman15@gmail.com>
2017-07-02 09:11:25 -0400

    Always force SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  force_ssl if: :ssl_configured?
   protect_from_forgery with: :exception
   helper :application
 
   before_action :authenticate_user!
+
+  private
+
+  def ssl_configured?
+    !Rails.env.development?
+  end
 end
